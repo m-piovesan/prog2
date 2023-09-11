373377f12659bd788a8ebab06a9a9938e0e5baf6
Matheus <matheusmoraespiovesan@gmail.com>
2023-09-10 22:08:58 -0300

    setting thing up

--- avalia02/arff.c
+++ avalia02/arff.c
@@ -3,23 +3,95 @@
 #include <string.h>
 #include "arff.h"
 
-void exibe_atributos(atributo *infos, int quantidade){
-  //Fun��o do A1 (com modifica��es para o atributo de categorias)
+//Função do A1 (com modificações para o atributo de categorias)
+void exibe_atributos(atributo *infos, int quantidade) {
+    if (infos == NULL) {
+        printf("O arquivo ARFF fornecido é inválido!\n");
+        exit(0);
+    }
+
+    printf("==== ATRIBUTOS DO ARQUIVO ====\n");
+    for (int i = 0; i < quantidade; i++) {
+        printf("-> Atributo #%d\n", i + 1);
+        printf("Rótulo: %s\n", infos[i].rotulo);
+        printf("Tipo: %s\n", infos[i].tipo);
+        if (infos[i].categorias) printf("Categorias: %s\n", infos[i].categorias);
+        if (i < quantidade - 1) printf("------------------------------\n");
+    }
+
+    printf("===============================\n");
 }
 
-int conta_atributos(FILE *arff){
-  //Fun��o do A1
+// Função do A1
+int conta_atributos(FILE *arff) {
+    int cont = 0;
+    char buffer[1024];
+
+    while (fgets(buffer, sizeof(buffer), arff) != NULL) { // lê uma linha do arquivo e armazena em buffer
+        if (strstr(buffer, "@data") != NULL) { // para se a linha contém "@data"
+            break;
+        } else if (strstr(buffer, "@attribute") == buffer) {
+            cont++;
+        }
+    }
+
+    return cont;
 }
 
-void processa_categorias(atributo *elemento, char *categorias){
-  //Recbe uma string com as categorias e atualiza o elemento com um vetor de strings (modificar a struct)
+// Recebe uma string com as categorias e atualiza o elemento com um vetor de strings 
+void processa_categorias(atributo *elemento, char *categorias) {
+
 }
 
-atributo* processa_atributos(FILE *arff, int quantidade){
-  //Fun��o do A1 (com modifica��es para o atributo de categorias)
+// Função do A1 (com modificações para o atributo de categorias)
+atributo* processa_atributos(FILE *arff, int quantidade) {
+    atributo *infos = malloc(quantidade * sizeof(atributo));
+
+    if (infos == NULL) {
+        perror("Erro ao alocar memória para os atributos");
+        return NULL;
+    }
+
+    char buffer[1024], rotulo[1024], tipo[1024], categorias[1024];
+
+    for (int i = 0; i < quantidade; i++) {
+        if (!fgets(buffer, sizeof(buffer), arff)) {
+            perror("Erro ao ler atributo");
+            free(infos);
+            return NULL;
+        }
+
+        if (strstr(buffer, "@attribute") == buffer) { // executa se a linha contém "@attribute"
+            if (sscanf(buffer, "@attribute %s %s", rotulo, tipo) >= 2) {
+                infos[i].rotulo = strdup(rotulo);
+                infos[i].tipo = strdup(tipo);
+
+                if (strcmp(tipo, "numeric") != 0 && strcmp(tipo, "string") != 0) { // executa se o tipo não for "numeric" nem "string"
+                    char *abre_chaves = strchr(buffer, '{'); // procura pela primeira ocorrência de "{"
+                    
+                    if (abre_chaves != NULL) {
+                        char *fecha_chaves = strchr(abre_chaves, '}'); // procura pela primeira ocorrência de "}"
+                        
+                        if (fecha_chaves != NULL) {
+                            *fecha_chaves = '\0';
+                            infos[i].categorias = strdup(abre_chaves + 1);
+                        }
+                    }
+                } else {
+                    infos[i].categorias = NULL;
+                }
+            } else {
+                continue;
+            }
+        }
+    }
+
+    return infos;
 }
 
-void valida_arff(FILE *arff, atributo *atributos, int quantidade){
-  //Recebe um arquivo arff com ponteiro de leitura antes do "@data"; passa por todas as linhas de dados e valida cada elementos de cada coluna em
-  //rela��o ao vetor de atributos tamb�m fornecido como argumento.
+/*  Recebe um arquivo ARFF com um ponteiro de leitura posicionado antes de @data
+    valida cada elemento em todas as linhas de dados em relação a um vetor de atributos
+    fornecido como argumento. */
+void valida_arff(FILE *arff, atributo *atributos, int quantidade) {
+
 }
